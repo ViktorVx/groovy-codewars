73d8dd7dcb427682f43951ff31984da0cac580c6
ViktorVx <victorptrv@yandex.ru>
2020-06-21 13:24:12 +0300

    DivisibleBy

---
+++ src/main/java/kyu8/DivisibleBy.groovy
@@ -0,0 +1,7 @@
+package kyu8
+
+class DivisibleBy {
+    static def isDivisible(n, x, y) {
+        n % x == 0 && n % y == 0
+    }
+}
--- src/test/groovy/kyu8/SubtractTheSumTests.groovy
+++ src/test/groovy/kyu8/SubtractTheSumTests.groovy
@@ -27,5 +27,16 @@ class SubtractTheSumTests extends Specification {
             42      | "everything"
             42 * 42 | "everything squared"
     }
+
+    @Test
+    def "Is n divisible by x and y?"(int n, int x, int y, boolean b) {
+        expect:
+            DivisibleBy.isDivisible(n, x, y) == b
+        where:
+            n  |  x  |  y  | b
+            3  |  3  |  4  | false
+            8  |  3  |  4  | false
+            12 |  3  |  4  | true
+    }
 }
 
