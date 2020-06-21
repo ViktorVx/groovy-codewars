347b649c821fe7d2fcc69913ec294afddba4f705
ViktorVx <victorptrv@yandex.ru>
2020-06-21 13:11:04 +0300

    CompareInts

---
+++ src/main/java/kyu8/CompareInts.groovy
@@ -0,0 +1,9 @@
+package kyu8
+
+class CompareInts {
+    static whatIs(x) {
+        if (x == 42) "everything"
+        else if (x == 42 * 42) "everything squared"
+        else "nothing"
+    }
+}
--- src/test/groovy/kyu8/SubtractTheSumTests.groovy
+++ src/test/groovy/kyu8/SubtractTheSumTests.groovy
@@ -14,5 +14,18 @@ class SubtractTheSumTests extends Specification {
             10   | "apple"
             8655 | "apple"
     }
+
+    @Test
+    def "How do I compare numbers?"(int n, String s) {
+        expect:
+            CompareInts.whatIs(n) == s
+        where:
+            n       | s
+            0       | "nothing"
+            123     | "nothing"
+            -1      | "nothing"
+            42      | "everything"
+            42 * 42 | "everything squared"
+    }
 }
 
